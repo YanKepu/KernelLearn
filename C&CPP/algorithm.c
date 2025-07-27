267adc220b3fb45dbf4fc5ce15694f15e7abd713
YanKepu <yan_kepu@163.com>
2025-07-27 10:13:12 +0800

    2025-0727

---
+++ C&CPP/algorithm.c
@@ -0,0 +1,19 @@
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 
+ * @param head ListNode类 
+ * @return ListNode类
+ */
+struct ListNode* ReverseList(struct ListNode* head ) {
+    // write code here 
+    ListNode *back = NULL;
+    back.next = NULL;
+    head->next   
+}
--- README.md
+++ README.md
@@ -14,3 +14,23 @@ Linux内核主要由几部分组成
 文件系统
 网络协议栈
 中断管理
+
+
+内核编译步骤：
+1、下载源码： 
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.14.tar.xz
+tar -xf linux-5.6.14.tar.xz
+cd linux-5.6.14
+
+2、保存当前内核配置的信息
+make menuconfig
+通过gedit config查看
+
+3、内核编译
+make
+
+4、模块安装操作
+
+sudo make modules_install
+
+5、重启系统查看内核是否编译成功
